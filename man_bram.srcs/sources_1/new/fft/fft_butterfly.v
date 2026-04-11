f5e77c08a9e7159968834e16c91614b8dcedc148
dylan-m-mitchell <dlnmtchll@gmail.com>
2026-04-10 22:30:34 -0500

    Refactor complex multiplier and FFT butterfly modules to use dynamic operation selection for FPU instances

--- man_bram.srcs/sources_1/new/fft/fft_butterfly.v
+++ man_bram.srcs/sources_1/new/fft/fft_butterfly.v
@@ -21,19 +21,23 @@ module fft_butterfly(
 	// -----------------------------------------------------------------------
     // Regs and wires
     // -----------------------------------------------------------------------
+	reg [1:0] fpu1_op, fpu2_op, fpu3_op, fpu4_op;
+	reg [63:0] xi_mux, xr_mux, yi_mux, yr_mux;
+	
 	wire[63:0] xi, xr, yi, yr;
 	wire [63:0] mult_resulti, mult_resultr;
 	
-	assign x_imag = xi;
-	assign x_real = xr;
-	assign y_imag = yi;
-	assign y_real = yr;
+	assign x_imag = xi_mux;
+	assign x_real = xr_mux;
+	assign y_imag = yi_mux;
+	assign y_real = yr_mux;
 	
 	// -----------------------------------------------------------------------
     // Operation encoding for FPUs
     // -----------------------------------------------------------------------
     localparam OP_ADD = 2'b00;
     localparam OP_SUB = 2'b01;
+    localparam OP_MUL = 2'b10;
 	
 	// -----------------------------------------------------------------------
     // Submodules
@@ -47,39 +51,45 @@ module fft_butterfly(
 		.outr_data		(mult_resultr)
 	);
 	
-	fp64_fpu #(
-		.STATIC_OP		(OP_SUB)
-	) fpu1(
+	fp64_fpu fpu1(
 		.a_data			(a_imag),
 		.b_data			(mult_resulti),
-		.op				(OP_SUB),
+		.op				(fpu1_op),
 		.result			(yi)
 	);
 	
-	fp64_fpu #(
-		.STATIC_OP		(OP_ADD)
-	) fpu2(
+	fp64_fpu fpu2(
 		.a_data			(a_imag),
 		.b_data			(mult_resulti),
-		.op				(OP_ADD),
+		.op				(fpu2_op),
 		.result			(xi)
 	);
 	
-	fp64_fpu #(
-		.STATIC_OP		(OP_SUB)
-	) fpu3(
+	fp64_fpu fpu3(
 		.a_data			(a_real),
 		.b_data			(mult_resultr),
-		.op				(OP_SUB),
+		.op				(fpu3_op),
 		.result			(yr)
 	);
 	
-	fp64_fpu #(
-		.STATIC_OP		(OP_ADD)
-	) fpu4(
+	fp64_fpu fpu4(
 		.a_data			(a_real),
 		.b_data			(mult_resultr),
-		.op				(OP_ADD),
+		.op				(fpu4_op),
 		.result			(xr)
 	);
+	
+	initial begin
+		fpu1_op = OP_SUB;
+		fpu2_op = OP_ADD;
+		fpu3_op = OP_SUB;
+		fpu4_op = OP_ADD;
+	end
+	
+	always @(*) begin
+		xi_mux = xi;
+		xr_mux = xr;
+		yi_mux = yi;
+		yr_mux = yr;
+	end
 endmodule
